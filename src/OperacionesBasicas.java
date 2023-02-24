65aa226e4771b14c0ea04056908dc9f5ab6321c8
Ruben Nuñez Gonzalez <rnunezgonzalez@danielcastelao.org>
2023-02-24 10:22:02 +0100

    Añadidas raices

--- src/OperacionesBasicas.java
+++ src/OperacionesBasicas.java
@@ -11,6 +11,7 @@ public class OperacionesBasicas {
     static final int SUBSTRACTION = 2;
     static  final int MULT = 3;
     static  final  int DIV = 4;
+    static final int RAIZ = 5;
     private OperacionesBasicas(){};
 
     /**
@@ -50,6 +51,21 @@ public class OperacionesBasicas {
                     System.out.println("DIV ERROR");
                 }
                 break;
+            case RAIZ:
+                float sqrt;
+                try{
+                    if(num2 == 2){
+                        sqrt = (float)Math.sqrt(num1);
+                        return sqrt;
+                    } else if (num2 == 3) {
+                        sqrt = (float)Math.cbrt(num1);
+                    } else{
+                        sqrt = (float)Math.pow(num1, 1.0 / num2);
+                        return sqrt;
+                    }
+                }catch (Exception e){
+                    System.out.println("RAIZ ERROR");
+                }
             default:
                 System.out.println("OPTION DOES NOT EXIST");
                 break;
